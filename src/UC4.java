0cfcfc82920777a30dc142161d022e40d11e1466
rohitswamy369 <gswamyroh@gmail.com>
2026-03-02 11:01:30 +0530

    intial project setup

---
+++ src/UC4.java
@@ -0,0 +1,15 @@
+package src;
+public class UC4 {
+    public static void main(String[] args) {
+        String[] banner = {
+            "*******  *******  ******   *******",
+            "*     *  *     *  *     *  *      ",
+            "*     *  *     *  ******   *******",
+            "*     *  *     *  *              *",
+            "*******  *******  *        *******"
+        };
+        for(String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
